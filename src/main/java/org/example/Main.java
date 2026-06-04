8ffff8a3e53738aa42cc1d95d51a88afb8d5e932
Carlos <ce882711@gmail.com>
2026-06-04 15:55:30 -0300

    Alteracao para master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Fazendo alteração aqui!"));
-
+        System.out.println("mudanca para master que nois fez");
     }
 }
